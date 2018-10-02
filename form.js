4d808b6bd020ff1336519f830635dc7e61de1f09
vaidesh shankar <42419232+vaideshshank@users.noreply.github.com>
2018-10-02 10:59:33 +0530

    updated express file

--- form.js
+++ form.js
@@ -1,22 +1,43 @@
+//middleware are thepieces of code that run before the other code in any node file.
+
+
+
 var express=require('express');
 var app=express();
 
 var bodyParser=require('body-parser');
+
+//Middlewares---
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/images',express.static(__dirname+"/images"));
+app.use('/images',express.static(__dirname+"/images"));		//middleware
+app.use('/',function(req,res,next){
+	console.log('This is the middleware.');
+	next();
+});
+
+//----
+
 app.set('view engine','ejs');
 
 
 app.get('/',function(req,res){
+	res.render('home');
+});
+
+app.get('/about',function(req,res){
+	res.render('about');
+});
+
+
+app.get('/form',function(req,res){
 	res.sendFile(__dirname+"/spare.html");
 	console.log(__dirname);
 	
 });
 
-app.post('/submit-student-data', function (req, res) {
-    var name=req.body.firstName+" "+req.body.lastName+"File : "+req.body.file;
-
-    res.render('ejsfile.ejs',{
+app.post('/form/submit-student-data', function (req, res) {
+    
+    res.render('ejsfile',{
 								firstname: req.body.firstName,
 								lastname: req.body.lastName,
 								hobbies:req.body.Hobbies
